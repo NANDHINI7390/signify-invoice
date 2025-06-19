596688c609caca5bf8999b8259d097e5946a7d7d
Nandy Gavas <nandygavas@gmail.com>
2025-06-19 08:29:42 +0000

    Fic

--- src/components/pages/InvoicePreviewPageClient.tsx
+++ src/components/pages/InvoicePreviewPageClient.tsx
@@ -109,7 +109,7 @@ export default function InvoicePreviewPageClient() {
         }
         setIsSending(false);
       }, 1500);
-      return; // Added missing return statement
+      return;
     }
 
     try {
